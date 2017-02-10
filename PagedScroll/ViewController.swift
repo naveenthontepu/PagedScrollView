74a5eac235d1a9cf66d3c6670309b5e200793ae0
naveen thontepu <tnaveen.leo@gmail.com>
2017-02-10 12:13:38 +0530

    Initial Commit

---
+++ PagedScroll/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PagedScroll
+//
+//  Created by Mac on 2/10/17.
+//  Copyright © 2017 naveen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
